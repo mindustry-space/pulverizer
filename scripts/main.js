01a85aab33e6ee2b6cd97ccc792927c90a72d867
Joel <joel@joel.tokyo>
2022-07-06 16:47:55 +1000

    feat: crush different content types to different files

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,43 @@
 const pulverizer = require("pulverizer");
 
 function main() {
-  let file = java.io.File(Version.combined().replace(/\s/g, "-") + ".json");
+  let file = java.io.File(Version.combined().replace(/\s/g, "-"));
+  file.mkdir();
 
   const displayName = "Pulverizer";
   const information = "Crushing content to " + file.getAbsolutePath() + ".";
   print(information);
 
   const callback = () => {
-    let writer = java.io.FileWriter(file.getAbsolutePath());
-    writer.write(JSON.stringify(pulverizer.crush(Vars.content.items())));
-    writer.close();
+    const content = Vars.content.getContentMap();
+    const skip = [
+      ContentType.block, // TODO
+      ContentType.mech_UNUSED,
+      ContentType.bullet, // TODO
+      ContentType.unit, // TODO
+      ContentType.effect_UNUSED,
+      ContentType.sector, // TODO
+      ContentType.loadout_UNUSED,
+      ContentType.typeid_UNUSED,
+      ContentType.error,
+      ContentType.planet, // TODO
+      ContentType.ammo_UNUSED,
+    ].map((v) => v.ordinal());
+    if (Object.keys(ContentType).includes("team")) {
+      skip.push(ContentType.team); // in development
+    }
+
+    for (let i = 0; i < ContentType.all.length; i++) {
+      if (skip.includes(i)) continue;
+
+      let type = ContentType.all[i].toString();
+      print("Crushing ContentType." + type + "...");
+      let writer = java.io.FileWriter(
+        file.getAbsolutePath() + "/" + type + ".json"
+      );
+      writer.write(JSON.stringify(pulverizer.crush(content[i])));
+      writer.close();
+    }
 
     Core.app.exit();
   };
