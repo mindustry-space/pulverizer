37a73692c58b93e44c9be45225f9c624fb04a250
Joel <joel@joel.tokyo>
2022-07-06 17:39:57 +1000

    feat: implement compressing/not recursing further into content scraped elsewhere

--- scripts/pulverizer.js
+++ scripts/pulverizer.js
@@ -1,27 +1,62 @@
-function crush(any) {
+function getContentClasses() {
+  if (Version.build >= 136) {
+    // enum contains classes
+    return ContentType.all.map((v) =>
+      v.contentClass ? v.contentClass.name : null
+    );
+  }
+
+  return [
+    "mindustry.type.Item",
+    "mindustry.world.Block",
+    "mindustry.entities.bullet.BulletType",
+    "mindustry.type.Liquid",
+    "mindustry.type.StatusEffect",
+    "mindustry.type.UnitType",
+    "mindustry.type.Weather",
+    "mindustry.type.SectorPreset",
+    "mindustry.type.Planet",
+    "mindustry.type.TeamEntry",
+  ];
+}
+
+const contentClasses = getContentClasses();
+
+function crush(any, doNotRecurseFurther) {
   switch (typeof any) {
     case "function":
-      return undefined;
+      return;
 
     case "object":
       if (any === null) return null;
 
-      if (typeof any["getClass"] === "function") return crushClass(any);
+      if (typeof any["getClass"] === "function")
+        return crushClass(any, doNotRecurseFurther);
 
-      return crushObject(any);
+      return crushObject(any, doNotRecurseFurther);
 
     case "undefined":
-      return undefined;
+      return;
 
     default:
       return any;
   }
 }
 
-function crushClass(obj) {
+function crushClass(obj, doNotRecurseFurther) {
   let actualType = obj.getClass();
   let type = actualType;
+  let isContentClass = false;
   while (true) {
+    if (contentClasses.includes(type.name)) {
+      isContentClass = true;
+    }
+    // log(type.name + " " + isContentClass + " " + doNotRecurseFurther);
+
+    if (doNotRecurseFurther && isContentClass) {
+      return obj.id;
+    }
+
     switch (type.name) {
       case "arc.struct.Seq": {
         // convert "object" into array
@@ -37,28 +72,28 @@ function crushClass(obj) {
       }
 
       case "java.lang.Class":
-        return undefined;
+        return;
 
       case "mindustry.content.TechTree$TechNode":
         // TODO
-        return undefined;
+        return;
 
       default: {
         type = type.getSuperclass();
         if (type === null) {
-          // print(actualType.name + " -> object");
-          return crushObject(obj);
+          if (isContentClass) return crushObject(obj, true);
+          return crushObject(obj, doNotRecurseFurther);
         }
       }
     }
   }
 }
 
-function crushObject(obj) {
+function crushObject(obj, doNotRecurseFurther) {
   let result = {};
   for (let k in obj) {
     // print("." + k);
-    let v = crush(obj[k]);
+    let v = crush(obj[k], doNotRecurseFurther);
     if (v !== undefined) {
       result[k] = v;
     }
